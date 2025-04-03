4d62a5b74f9098250f2d51dce4ce5d1fdc02957e
A N M Mashud <165895860+anmmashud@users.noreply.github.com>
2025-04-03 09:28:46 +0000

    Update app.py

--- app.py
+++ app.py
@@ -79,27 +79,3 @@ bmi_categories.index = bmi_categories.index + 1
 # Display BMI category table
 st.table(bmi_categories)
 
-# Show a spinner during a process
-with st.spinner(text="In progress"):
-    time.sleep(3)
-    st.success("Done")
-
-# Show and update progress bar
-bar = st.progress(50)
-time.sleep(3)
-bar.progress(100)
-
-with st.status("Authenticating...") as s:
-    time.sleep(2)
-    st.write("Some long response.")
-    s.update(label="Response")
-
-st.balloons()
-st.snow()
-st.toast("Warming up...")
-st.error("Error message")
-st.warning("Warning message")
-st.info("Info message")
-st.success("Success message")
-st.exception(e)
-
